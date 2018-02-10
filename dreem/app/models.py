145091f38faa8f30ff8231968d9c8a3aa7108f9f
mactov <mactov@iablaka.com>
2018-02-10 20:02:32 +0100

    ready to push to github

--- dreem/app/models.py
+++ dreem/app/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 
 class User(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=50)
 
 
 class Device(models.Model):
-    name = models.CharField()
-    users = models.ManyToManyField(Users)
+    name = models.CharField(max_length=50)
+    users = models.ManyToManyField(User)
 
 
 class Record(models.Model):
-    file = models.CharField()	
+    file = models.CharField(max_length=50)	
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     device = models.ForeignKey(Device, on_delete=models.CASCADE)
